c4c9ad621511ffd966645e908b4d407b216ca8ea
kseta <keigo.seta@gmail.com>
2017-05-10 11:18:54 +0900

    Fix deploy.php for deployer v5

--- deploy.php
+++ deploy.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace Deployer;
 
-require 'recipe/symfony.php';
+require 'recipe/symfony3.php';
 
 // Configuration
 
-set('repository', 'git@domain.com:username/repository.git');
+set('repository', 'git@github.com:kseta/demo-deploy.git');
 set('git_tty', true); // [Optional] Allocate tty for git on first deployment
 add('shared_files', []);
 add('shared_dirs', []);
@@ -13,13 +14,17 @@ add('writable_dirs', []);
 
 // Hosts
 
-host('project.com')
-    ->stage('production')
-    ->set('deploy_path', '/var/www/project.com');
-    
-host('beta.project.com')
-    ->stage('beta')
-    ->set('deploy_path', '/var/www/project.com');  
+//host('project.com')
+//    ->stage('production')
+//    ->set('deploy_path', '/var/www/project.com');
+
+host('127.0.0.1')
+    ->stage('staging')
+    ->user('ubuntu')
+    ->port('2230')
+    ->identityFile('/Users/kseta/.ghq/github.com/kseta/demo-deploy/.vagrant/machines/staging/virtualbox/private_key')
+    ->forwardAgent(true)
+    ->set('deploy_path', '~/demo.com');
 
 
 // Tasks
@@ -30,11 +35,11 @@ task('php-fpm:restart', function () {
     // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
     run('sudo systemctl restart php-fpm.service');
 });
-after('deploy:symlink', 'php-fpm:restart');
+//after('deploy:symlink', 'php-fpm:restart');
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
 
-before('deploy:symlink', 'database:migrate');
+//before('deploy:symlink', 'database:migrate');
